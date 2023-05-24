1a51547c139887a11420942e3f4f417dae609838
Wei Wang <wvw@google.com>
2023-05-24 14:08:07 +0700

    sm8250-common: Boot time tuning
    Test: build and boottime test
    Change-Id: Ib8f31d39501b4747f0c268d2dd87e814bcc7843b

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -490,6 +490,20 @@ echo 128 > /sys/block/dm-7/queue/read_ahead_kb
 echo 128 > /sys/block/dm-8/queue/read_ahead_kb
 echo 128 > /sys/block/dm-9/queue/read_ahead_kb
 
+# Setup runtime schedTune
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 0 > /dev/stune/foreground/schedtune.prefer_high_cap
+echo 0 > /dev/stune/foreground/schedtune.boost
+echo 0 > /dev/stune/schedtune.prefer_idle
+echo 0 > /dev/stune/schedtune.prefer_high_cap
+echo 0 > /dev/stune/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
+echo 10 > /dev/stune/top-app/schedtune.boost 10
+
 # Post-setup services
 case "$target" in
     "msm8660" | "msm8960" | "msm8226" | "msm8610" | "mpq8092" )
